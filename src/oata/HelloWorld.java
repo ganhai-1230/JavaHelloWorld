14b261467c9447c501ea4d6c94c87611515a43e8
ganhai-1230 <gh.1230@icloud.com>
2016-10-25 14:36:32 +0800

    fix

--- src/oata/HelloWorld.java
+++ src/oata/HelloWorld.java
@@ -1,18 +1,14 @@
 package oata;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.BasicConfigurator;
+
 public class HelloWorld {
+	static Logger logger = Logger.getLogger(HelloWorld.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+    	BasicConfigurator.configure();
+    	logger.info("Hello World");		// the old SysO-statement
     }
 }
 
-public class HelloWorldTest extends junit.framework.TestCase {
-
-    public void testNothing() {
-    }
-    
-    public void testWillAlwaysFail() {
-        fail("An error message");
-    }
-    
-}
